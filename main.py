4880006dfbe35aaca9eaf5beb7d4886831b0fe3b
sn0wd3n <63225928+ms3c@users.noreply.github.com>
2020-10-04 12:27:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import random as rand
+
 print("Hello World")
+
+radnInt = rand.radnint(1, 10)
+while(1):
+  '''you might need to do casting!'''
+  guess = int(input("Enter secret number: "))
+  if guess < randInt:
+    print('Too low')
+  elif guess > randInt:
+    print('Too high')
+  elif guess  == randInt:
+    print('Congratulation!!')
+  else:
+    print('Unknown input:)
